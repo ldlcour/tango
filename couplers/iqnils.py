0c62c10913e0a54670adc532a50f8e7877f615ba
joris <Joris.Degroote@UGent.be>
2018-04-06 16:37:43 +0200

    Bugfixes in case tube1d and IQN-ILS.

--- couplers/iqnils.py
+++ couplers/iqnils.py
@@ -16,16 +16,19 @@ class IQNILS:
         self.v = np.matrix([])
         self.w = np.matrix([])
         self.minsignificant = parameters["minsignificant"]
+        self.omega = parameters["omega"]
 
     def add(self, x, xt):
         r = xt - x
         if self.added:
+            dr = r - self.rref
+            dxt = xt-self.xtref
             if self.v.shape[1]:
-                self.v = np.concatenate((r-self.rref, self.v), 1)
-                self.w = np.concatenate((xt-self.xtref, self.w), 1)
+                self.v = np.concatenate((dr, self.v), 1)
+                self.w = np.concatenate((dxt, self.w), 1)
             else:
-                self.v = r-self.rref
-                self.w = xt-self.xtref
+                self.v = np.matrix(np.reshape(dr, (-1, 1)))
+                self.w = np.matrix(np.reshape(dxt, (-1, 1)))
         self.rref = r
         self.xtref = np.array(xt)
         self.added = True
@@ -33,6 +36,7 @@ class IQNILS:
     def predict(self, r):
         # Remove columns resulting in small diagonal elements in R
         singular = True
+        print(self.v.shape)
         while singular and self.v.shape[1]:
             rr = qr(self.v, mode='r')
             diag = np.diagonal(rr)
@@ -46,12 +50,13 @@ class IQNILS:
         # Calculate return value if sufficient data available
         if self.v.shape[1]:
             # Interface Quasi-Newton with approximation for the inverse of the Jacobian from a least-squares model
-            qq, rr = qr(self.v)
+            qq, rr = qr(self.v, mode='reduced')
+            print(rr.shape)
             c = solve_triangular(rr, np.transpose(qq) * (-r))
             dx = self.w * c + r
         else:
             if self.added:
-                dx = np.zeros_like(self.xtref)
+                dx = self.omega * r
             else:
                 raise RuntimeError("No information to predict")
         return dx
@@ -63,6 +68,7 @@ class IQNILS:
         self.w = np.matrix([])
 
     def finalizestep(self):
+        print(self.added)
         if self.added:
             self.added = False
         else:
